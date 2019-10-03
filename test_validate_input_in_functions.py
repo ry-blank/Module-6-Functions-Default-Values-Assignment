054fc6696bc498c96008dbb6adf25b7d81f76aa3
ry-blank <52143833+ry-blank@users.noreply.github.com>
2019-10-03 15:59:12 -0500

    Functions Default Values Assignment
    Initial Commit

---
+++ test_validate_input_in_functions.py
@@ -0,0 +1,27 @@
+import unittest
+from more_functions import validate_input_in_functions as validate
+
+
+class MyTestCase(unittest.TestCase):
+    def test_score_input_test_name(self):
+        self.assertEqual(validate.score_input("TestScore"), 'TestScore: 0')
+
+    def test_score_input_test_score_valid(self):
+        self.assertEqual(validate.score_input("UnitTest", test_score=50), 'UnitTest: 50')
+
+    def test_score_input_test_score_below_range(self):
+        self.assertEqual(validate.score_input("UnitTest", -50), ValueError)
+
+    def test_score_input_test_score_above_range(self):
+        self.assertEqual(validate.score_input("UnitTest", 150), ValueError)
+
+    def test_test_score_non_numeric(self):
+        self.assertEqual(validate.score_input("UnitTest", 'Value'), ValueError)
+
+    # all args test
+    def test_score_input_invalid_message(self):
+        self.assertEqual(validate.score_input("UnitTest", 'Invalid'), 'Invalid test score, try again!')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ validate_input_in_functions.py
@@ -0,0 +1,23 @@
+"""
+Program: inner_functions_assignment
+Author: Ryan Blankenship
+Last Date Modified: 10/1/2019
+
+The purpose of this program is to calculate the
+area and the perimeter of a rectangle from a
+list of numbers.
+"""
+
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
+    """
+    :param test_name: name input by user
+    :param test_score: score input by user
+    :param invalid_message: message for bad input
+    :return:
+    """
+    # return { test_name: test_score}
+    pass
+
+
+if __name__ == '__main__':
+    score_input()
